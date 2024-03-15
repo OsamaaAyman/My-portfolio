fe86e49b888043e2e28b05eb9b7239b33c11c3f9
Osamaaaayman <osamaaaaymen@gmail.com>
2024-03-15 05:39:57 +0200

    Nav Java

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-let hamburger =document.querySelector(".hamburger");
-hamburger.addEventListener("click", function(){
-    hamburger.classList.toggle("active")
-    let navb= document.querySelector(".links")
-    navb.classList.toggle("active")
-    let navli=document.querySelectorAll("a")
-    for(let i=0; i<navli.length;i++){
-        navli[i].addEventListener("click", function (){
-            navb.classList.remove("active");
-            hamburger.classList.remove("active");
-        })
-    }
-})
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  let overlay = document.querySelector(".overlay");
+  let navb = document.querySelector(".links");
+  let navli = document.querySelectorAll("a");
+
+  hamburger.classList.toggle("active");
+  navb.classList.toggle("active");
+  overlay.classList.toggle("active");
+
+  if(navb.classList.contains('active')){
+    document.body.style.position = 'fixed'
+  }
+
+  const removeNav = function () {
+    overlay.classList.remove("active");
+    navb.classList.remove("active");
+    hamburger.classList.remove("active");
+    document.body.style.position = 'static'
+  };
+  overlay.onclick = function () {
+    removeNav();
+  };
+  
+  navli.forEach(li => li.addEventListener('click', removeNav));
+});
